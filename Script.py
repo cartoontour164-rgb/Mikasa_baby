4612e763bda66b1d3d3aa1b968d7147445323629
cartoontour164-rgb <cartoontour164@gmail.com>
2026-02-19 06:58:33 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -224,7 +224,7 @@ Nᴀᴍᴇ : {}
 
 Mᴇꜱꜱᴀɢᴇ : <b>{}</b>"""
     
-    CAPTION = """<b><a href="https://t.me/updates_op">{file_name}</a></b>\n\n<b>⚜️ Powered By : <a href="https://t.me/iP_Update">[ iP Update ]</a></b>"""
+    CAPTION = """<b><a href="https://t.me/updates_op">{file_name}</a></b>\n\n<b>⚜️ Powered By : <a href="https://t.me/scout_regimant">[ Scout Regiment ]</a></b>"""
 
     
     MOVIE_UPDATE_NOTIFY_TXT = """
