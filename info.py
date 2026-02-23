2f4dcb255d3b6f51341a7657e7d0699a774bbd09
cartoontour164-rgb <cartoontour164@gmail.com>
2026-02-23 21:27:29 +0530

    Update info.py

--- info.py
+++ info.py
@@ -120,7 +120,7 @@ THREE_VERIFY_GAP = int(environ.get('THREE_VERIFY_GAP', "54000"))
 # ============================
 # Channel & Group Links Configuration
 # ============================
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/mikasaxchatting') # Group link for the bot
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/entertainment_hab') # Group link for the bot
 OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/Mikasa_devbot') # Owner link for the bot
 UPDATE_CHNL_LNK = environ.get('UPDATE_CHNL_LNK', 'https://t.me/updates_op') # Update channel link for the bot
 
